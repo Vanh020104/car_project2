4be071d7a733bfa7020f0c2557b2a65910ec50e6
luongmanh1603 <114908089+luongmanh1603@users.noreply.github.com>
2023-11-12 14:23:49 +0700

    feedback

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,12 @@ use Carbon\Carbon;
 |
 */
 Route::get('/',[App\Http\Controllers\HomeController::class,"home"]);
-Route::get('/detail/{product:slug}', [\App\Http\Controllers\HomeController::class,"product"]);
+Route::get('/detail/{product:slug}', [\App\Http\Controllers\HomeController::class,"product"])->name('product.detail');
 Route::get('/category/{category:slug}', [\App\Http\Controllers\HomeController::class,"category"]);
 
 Route::get('/errors', [\App\Http\Controllers\HomeController::class, "Errors"])->name("errors");
+Route::get('/cars_list', [\App\Http\Controllers\HomeController::class,"cars_list"]);
+Route::get('/filters', [\App\Http\Controllers\HomeController::class,"filterProducts"]);
 Route::get('/car_list/{category:slug}', [\App\Http\Controllers\HomeController::class,"cars"]);
 Route::get('/filter/{category:slug}', [\App\Http\Controllers\HomeController::class,"filterProduct"]);
 
@@ -32,6 +34,10 @@ Route::middleware("auth")->group(function (){
     Route::get('/clear-cart', [\App\Http\Controllers\HomeController::class, "clearCart"]);
     Route::post('/checkout', [\App\Http\Controllers\HomeController::class,"placeOrder"]);
     Route::get('/thank-you/{order}', [\App\Http\Controllers\HomeController::class,"thankYou"]);
+    Route::get('/feedback/{order}', [\App\Http\Controllers\HomeController::class,"feedback"]);
+    Route::post('/feedbacks', [\App\Http\Controllers\HomeController::class,"placefeedback"]);
+
+
     Route::get('/checkout', [\App\Http\Controllers\HomeController::class,"checkout"])->name('checkout');
     Route::get('/paypal-success/{order}', [\App\Http\Controllers\HomeController::class,"paypalSuccess"]);
     Route::get('/paypal-cancel/{order}', [\App\Http\Controllers\HomeController::class,"paypalCancel"]);
