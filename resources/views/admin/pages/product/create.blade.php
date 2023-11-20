2801d8cac900fe3b0240f92496266eeab29f3f81
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2023-11-20 08:56:57 +0700

    create car

--- resources/views/admin/pages/product/create.blade.php
+++ resources/views/admin/pages/product/create.blade.php
@@ -1,6 +1,6 @@
 @extends("admin.layouts.admin_app")
 @section("content")
-    <div class="card card-primary">
+    <div class="card card-primary" style="height: 2050px">
         <!-- form start -->
         <form class="form_create" action="{{url("admin/product/create")}}" method="post" enctype="multipart/form-data">
             <h2 style="margin-top:-20px;text-align: center;color: #1a1af8;font-size: 28px;margin-bottom: 20px" class="text-xl font-bold text-black dark:text-white">
@@ -10,177 +10,217 @@
             <div class="card-body">
              <div class="ttcar1">
                  <div class="form-group">
-                     <label for="exampleInputEmail1">Name</label>
-                     <input style="margin-left: 15px" type="text" name="name" value="{{old("name")}}" class="form-control"  placeholder="Enter Name" required>
+                     <label for="exampleInputEmail1">Name Car</label><br>
+                     <input class="name_car"  type="text" name="name" value="{{old("name")}}"   placeholder="Enter Name" required>
                      @error("name")
                      <p class="text-danger"><i>{{$message}}</i></p>
                      @enderror
                  </div>
-                 <div class="form-group">
-                     <label>Category</label>
-                     <select style="width: 200px"  name="category_id" class="form-control">
-                         <option value="">Choose category</option>
-                         @foreach($categories as $item)
-                             <option @if($item->id==old("category_id")) selected @endif value="{{$item->id}}">{{$item->name}}</option>
-                         @endforeach
-                     </select>
-                     @error("category_id")
-                     <p class="text-danger"><i>{{$message}}</i></p>
-                     @enderror
-                 </div>
-                 <div class="form-group">
-                     <label for="exampleInputPassword1">Price</label>
-                     <input style="margin-left: 25px" type="number" value="{{old("price")}}" name="price" class="form-control"  placeholder="Price">
-                     @error("price")
-                     <p class="text-danger"><i>{{$message}}</i></p>
-                     @enderror
-                 </div>
-                 <div class="form-group">
-                     <label for="exampleInputPassword1">
-                         Hourly Price</label>
-                     <input style="margin-left: 25px" type="number" value="{{old("hourly_price")}}" name="hourly_price" class="form-control"  placeholder="Price">
-                     @error("hourly_price")
-                     <p class="text-danger"><i>{{$message}}</i></p>
-                     @enderror
-                 </div>
-                 <div class="form-group">
-                     <label for="exampleInputPassword1">Deposit</label>
-                     <input style="margin-left: 7px" type="number" value="{{old("deposit")}}" name="deposit" class="form-control"  placeholder="Deposit">
-                     @error("deposit")
-                     <p class="text-danger"><i>{{$message}}</i></p>
-                     @enderror
-                 </div>
-                 <div class="form-group">
-                     <label for="exampleInputFile">Thumbnail</label>
-                     <div class="input-group">
-                         <div class="custom-file">
-                             <input style="height: 33px;width: 300px"  name="thumbnail" type="file" class="custom-file-input" id="exampleInputFile">
-                             <label style="margin-left: 20px" class="custom-file-label" for="exampleInputFile">Choose file</label>
-                         </div>
+
+                 <div style="display: flex; margin-top: 20px; justify-content: space-between;gap: 20px">
+                     <div class="form-group">
+                         <label>Category</label> <br>
+                         <select class="category_form" name="category_id" >
+                             <option value="">Category</option>
+                             @foreach($categories as $item)
+                                 <option @if($item->id==old("category_id")) selected @endif value="{{$item->id}}">{{$item->name}}</option>
+                             @endforeach
+                         </select><br>
+                         @error("category_id")
+                         <p class="text-danger"><i>{{$message}}</i></p>
+                         @enderror
+                     </div>
+                     <div class="form-group">
+                         <label for="exampleInputPassword1">Price</label> <br>
+                         <input class="price_create" type="number" value="{{old("price")}}" name="price"   placeholder="Price" min="300"><br>
+                         @error("price")
+                         <p class="text-danger"><i>{{$message}}</i></p>
+                         @enderror
                      </div>
                  </div>
-                 <div class="form-group">
-                     <label>Qty</label>
-                     <input style="margin-left: 40px" type="number" value="{{old("qty")}}" name="qty" class="form-control"  placeholder="Qty">
-                     @error("qty")
-                     <p class="text-danger"><i>{{$message}}</i></p>
-                     @enderror
+
+
+                 <div style="display: flex; margin-top: 20px; justify-content: space-between;">
+                     <div class="form-group">
+                         <label for="exampleInputPassword1">Hourly Price</label><br>
+                         <input style="width: 220px;" class="hourly_price" type="number" value="{{old("hourly_price")}}" name="hourly_price" placeholder="Hourly Price" min="250">
+                         <br>
+                         @error("hourly_price")
+                         <p class="text-danger"><i>{{$message}}</i></p>
+                         @enderror
+                     </div>
+                     <div class="form-group">
+                         <label for="exampleInputPassword1">Deposit</label><br>
+                         <input style="width: 220px;" class="hourly_price" type="number" value="{{old("deposit")}}" name="deposit"  placeholder="Deposit" min="1200"><br>
+                         @error("deposit")
+                         <p class="text-danger"><i>{{$message}}</i></p>
+                         @enderror
+                     </div>
                  </div>
 
-                 <div class="form-group">
-                     <label style="position: absolute">Description</label>
-                     <textarea style="width:270px;border-radius: 8px;margin-top: 30px;background-color: #eceaea;" name="description" class="form-control" row="5">
+                 <div style="display: flex; margin-top: 20px; justify-content: space-between;">
+
+                     <div class="form-group">
+                         <label>Qty</label><br>
+                         <input class="qty_car" type="number" value="{{old("qty")}}" name="qty" placeholder="Qty" min="1"><br>
+                         @error("qty")
+                         <p class="text-danger"><i>{{$message}}</i></p>
+                         @enderror
+                     </div>
+                     <div class="form-group">
+                         <label for="exampleInputFile">Thumbnail</label><br>
+                         <input style="width: 220px;" name="thumbnail" type="file" id="exampleInputFile">
+                     </div>
+                 </div>
+
+                 <div class="form-group" style="margin-top: 20px">
+                     <label>Description</label> <br>
+                     <textarea style="border-radius: 4px;background-color: #f8f9fa;width: 100%; padding: 0 10px" name="description"  row="5">
                         {{old("description")}}
                     </textarea>
                  </div>
              </div>
-                <div style="margin-left: 40px" class="ttcar2">
-                    <div class="form-group">
-                        <label>Seat</label>
-                        <input type="number" value="{{old("seat")}}" name="seat" class="form-control"  placeholder="Seat">
-                        @error("seat")
-                        <p class="text-danger"><i>{{$message}}</i></p>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label>Door</label>
-                        <input type="number" value="{{old("door")}}" name="door" class="form-control"  placeholder="Door">
-                        @error("door")
-                        <p class="text-danger"><i>{{$message}}</i></p>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label>Car Year</label>
-                        <input type="number" value="{{old("car_year")}}" name="car_year" class="form-control"  placeholder="Car Year">
-                        @error("car_year")
-                        <p class="text-danger"><i>{{$message}}</i></p>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label>Mileage</label>
-                        <input type="number" value="{{old("mileage")}}" name="mileage" class="form-control"  placeholder="Mileage">
-                        @error("mileage")
-                        <p class="text-danger"><i>{{$message}}</i></p>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label>Horsepower</label>
-                        <input type="number" value="{{old("power")}}" name="power" class="form-control"  placeholder="Horsepower">
-                        @error("power")
-                        <p class="text-danger"><i>{{$message}}</i></p>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Fuel Style</label>
-                        <input type="text" name="fuel_style" value="{{old("fuel_style")}}" class="form-control"  placeholder="Fuel Style" required>
-                        @error("fuel_style")
-                        <p class="text-danger"><i>{{$message}}</i></p>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Color</label>
-                        <input type="text" name="color" value="{{old("color")}}" class="form-control"  placeholder="Color" required>
-                        @error("color")
-                        <p class="text-danger"><i>{{$message}}</i></p>
-                        @enderror
+
+
+
+                <div class="ttcar2" style="margin-top: 20px; display: flex;  justify-content: space-between;">
+                    <div style="display: grid">
+                        <div class="form-group">
+                            <label>Seat</label><br>
+                            <input type="number" value="{{old("seat")}}" name="seat"  placeholder="Seat" min="2"><br>
+                            @error("seat")
+                            <p class="text-danger"><i>{{$message}}</i></p>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Door</label><br>
+                            <input type="number" value="{{old("door")}}" name="door" placeholder="Door" min="2"><br>
+                            @error("door")
+                            <p class="text-danger"><i>{{$message}}</i></p>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Car Year</label><br>
+                            <input type="number" value="{{old("car_year")}}" name="car_year" placeholder="Car Year" min="2010"><br>
+                            @error("car_year")
+                            <p class="text-danger"><i>{{$message}}</i></p>
+                            @enderror
+                        </div>
+
                     </div>
 
+                    <div style="display: grid">
+                        <div class="form-group">
+                            <label>Horsepower</label><br>
+                            <input type="number" value="{{old("power")}}" name="power"  placeholder="Horsepower" min="534"><br>
+                            @error("power")
+                            <p class="text-danger"><i>{{$message}}</i></p>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Fuel Style</label><br>
+                            <input type="text" name="fuel_style" value="{{old("fuel_style")}}"  placeholder="Fuel Style" required><br>
+                            @error("fuel_style")
+                            <p class="text-danger"><i>{{$message}}</i></p>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Color</label><br>
+                            <input type="text" name="color" value="{{old("color")}}"   placeholder="Color" required><br>
+                            @error("color")
+                            <p class="text-danger"><i>{{$message}}</i></p>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Mileage</label><br>
+                    <input type="number" class="mileage" value="{{old("mileage")}}" name="mileage"  placeholder="Mileage" min="200"><br>
+                    @error("mileage")
+                    <p class="text-danger"><i>{{$message}}</i></p>
+                    @enderror
                 </div>
 
             </div>
             <!-- /.card-body -->
-            <div class="card-footer">
-                <button type="submit" class="btn btn-primary" id="btn">Submit</button>
+            <div class=" btn-fullwidth">
+                <button type="submit" class=" btn_fullwidth" id="btn">Submit</button>
             </div>
         </form>
     </div>
 
 @endsection
+
 <style>
     .form_create{
         border: #f2f2f2 solid 1px;
         border-radius: 8px;
-        margin-top: 6%;
-        margin-bottom: 10%;
-        margin-left: 25%;
-        padding: 80px;
+        margin-left: 45%;
+        padding: 40px 80px;
         background: #ffffff;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         color:#333333;
+        width: 100%;
     }
-    .card-body{
-        display: flex;
-    }
-    .card-footer{
-        text-align: center;
-        margin-top: 30px;
+    .name_car {
+       border: 1px solid #999999;
+        width: 100%;
+        border-radius: 3px;
+        height: 30px;
+        padding: 10px 20px;
 
     }
-    #btn{
-        width: 300px;
-        background-color: #4d4df8;
+
+    .category_form {
+        border: 1px solid #999999;
+        width: 220px;
+        border-radius: 3px;
+        height: 30px;
+        padding: 5px 20px;
+        color: #999999;
+    }
+    .price_create {
+        border: 1px solid #999999;
+        width: 220px;
+        border-radius: 3px;
+        height: 30px;
+        padding: 10px 20px;
+
+    }
+
+    .hourly_price {
+        border: 1px solid #999999;
+        border-radius: 3px;
+        height: 30px;
+        padding: 10px 20px;
+
+    }
+
+    .qty_car {
+        border: 1px solid #999999;
+        border-radius: 3px;
+        height: 30px;
+        padding: 10px 20px;
+        width: 220px;
+    }
+
+    .ttcar2 input, .mileage{
+        border: 1px solid #999999;
+        border-radius: 3px;
+        height: 30px;
+        padding: 10px 20px;
+        width: 220px;
+    }
+    .btn-fullwidth {
+        margin-top: 20px;
+        background-color: #3c50e0;
+        border-radius: 4px;
+        width: 150px;
+        margin-left: 36%;
+    }
+    .btn_fullwidth {
+        padding: 10px 50px;
         color: white;
-        padding-top: 8px;
-        padding-bottom: 8px;
-        border-radius: 8px;
     }
-    #btn:hover{
-        background-color: #1e1ef8;
-    }
-    input{
-        border-radius: 8px;
-        padding-left: 50px;
-    }
-    .ttcar1 .form-group{
-        margin-bottom: 10px;
-    }
-    .ttcar1 input{
-        background-color: #eceaea;
-        padding-left: 20px;
-    }
-    .ttcar2 input{
-        padding-left: 20px;
-        background-color: #eceaea;
-    }
-
 </style>
+
